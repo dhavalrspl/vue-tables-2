7b5e2ae47c3f3aa9d20a59666bda3283de6fe153
Dhaval.Patel <dhaval.patel@rishabhsoft.com>
2017-09-01 14:43:13 +0530

    Created the build.

--- compiled/template/footer-headings.js
+++ compiled/template/footer-headings.js
@@ -1,6 +1,12 @@
 "use strict";
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });
+  } else {
+    obj[key] = value;
+  }return obj;
+}
 
 module.exports = function (h, that) {
 
@@ -10,44 +16,48 @@ module.exports = function (h, that) {
 
   if (that.opts.footerHeadings) {
 
-    var columns = that.allColumns;
+    // var columns = that.allColumns;
 
-    columns.map(function (column) {
+    that.opts.footerHeadings.map(function (column) {
+      // footerHeadings.push(h(
+      //   "th",
+      //   {
+      //     on: {
+      //       click: that.orderByColumn.bind(that, column)
+      //     },
+      //     "class": that.sortableClass(column) },
+      //   [h(
+      //     "span",
+      //     { "class": "VueTables__heading" },
+      //     [that.getHeading(column)]
+      //   ), h(
+      //     "span",
+      //     _defineProperty({
+      //       directives: [{
+      //         name: "show",
+      //         value: that.sortable(column)
+      //       }],
 
-      footerHeadings.push(h(
-        "th",
-        {
-          on: {
-            click: that.orderByColumn.bind(that, column)
-          },
-          "class": that.sortableClass(column) },
-        [h(
-          "span",
-          { "class": "VueTables__heading" },
-          [that.getHeading(column)]
-        ), h(
-          "span",
-          _defineProperty({
-            directives: [{
-              name: "show",
-              value: that.sortable(column)
-            }],
+      //       "class": "VueTables__sort-icon pull-right"
+      //     }, "class", that.sortableChevronClass(column)),
+      //     []
+      //   )]
+      // ));
 
-            "class": "VueTables__sort-icon pull-right"
-          }, "class", that.sortableChevronClass(column)),
-          []
-        )]
-      ));
+      /**
+       * Modify by Dhaval Patel
+       * Date: 28-Aug-2017
+       * 
+       * To Make it fixed footer row.
+       */
+      footerHeadings.push(h("th", {}, [h("span", { "class": "VueTables__heading" }, [that.getHeading(column)]), h("span", _defineProperty({
+        directives: [{
+          name: "show"
+        }]
+
+      }, "class", that.sortableChevronClass(column)), [])]));
     });
   }
 
-  return h(
-    "tfoot",
-    null,
-    [h(
-      "tr",
-      null,
-      [footerHeadings]
-    )]
-  );
+  return h("tfoot", null, [h("tr", null, [footerHeadings])]);
 };
